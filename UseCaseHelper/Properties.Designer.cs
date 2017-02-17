1cd96605b7f073e4d6ed4c08defb47dadf055975
mrtsven <zahharov@hotmail.com>
2017-02-17 10:48:35 +0100

    created settings

---
+++ UseCaseHelper/Properties.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace UseCaseHelper
+{
+    partial class Properties
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNaam = new System.Windows.Forms.TextBox();
+            this.txtSamen = new System.Windows.Forms.TextBox();
+            this.txtActoren = new System.Windows.Forms.TextBox();
+            this.txtPre = new System.Windows.Forms.TextBox();
+            this.txtPost = new System.Windows.Forms.TextBox();
+            this.txtBeschrijving = new System.Windows.Forms.RichTextBox();
+            this.txtUitzondering = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNaam
+            // 
+            this.txtNaam.Location = new System.Drawing.Point(112, 22);
+            this.txtNaam.Name = "txtNaam";
+            this.txtNaam.Size = new System.Drawing.Size(412, 22);
+            this.txtNaam.TabIndex = 0;
+            // 
+            // txtSamen
+            // 
+            this.txtSamen.Location = new System.Drawing.Point(112, 50);
+            this.txtSamen.Name = "txtSamen";
+            this.txtSamen.Size = new System.Drawing.Size(412, 22);
+            this.txtSamen.TabIndex = 1;
+            // 
+            // txtActoren
+            // 
+            this.txtActoren.Location = new System.Drawing.Point(112, 78);
+            this.txtActoren.Name = "txtActoren";
+            this.txtActoren.Size = new System.Drawing.Size(412, 22);
+            this.txtActoren.TabIndex = 2;
+            // 
+            // txtPre
+            // 
+            this.txtPre.Location = new System.Drawing.Point(112, 106);
+            this.txtPre.Name = "txtPre";
+            this.txtPre.Size = new System.Drawing.Size(412, 22);
+            this.txtPre.TabIndex = 3;
+            // 
+            // txtPost
+            // 
+            this.txtPost.Location = new System.Drawing.Point(112, 454);
+            this.txtPost.Name = "txtPost";
+            this.txtPost.Size = new System.Drawing.Size(412, 22);
+            this.txtPost.TabIndex = 4;
+            // 
+            // txtBeschrijving
+            // 
+            this.txtBeschrijving.Location = new System.Drawing.Point(112, 134);
+            this.txtBeschrijving.Name = "txtBeschrijving";
+            this.txtBeschrijving.Size = new System.Drawing.Size(412, 145);
+            this.txtBeschrijving.TabIndex = 5;
+            this.txtBeschrijving.Text = "";
+            // 
+            // txtUitzondering
+            // 
+            this.txtUitzondering.Location = new System.Drawing.Point(112, 285);
+            this.txtUitzondering.Name = "txtUitzondering";
+            this.txtUitzondering.Size = new System.Drawing.Size(412, 145);
+            this.txtUitzondering.TabIndex = 6;
+            this.txtUitzondering.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Naam";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Samenvatting";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Actoren";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Preconditie";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Beschrijving";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(2, 288);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Uitzonderingen";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 454);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Postconditie";
+            // 
+            // Properties
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 488);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUitzondering);
+            this.Controls.Add(this.txtBeschrijving);
+            this.Controls.Add(this.txtPost);
+            this.Controls.Add(this.txtPre);
+            this.Controls.Add(this.txtActoren);
+            this.Controls.Add(this.txtSamen);
+            this.Controls.Add(this.txtNaam);
+            this.Name = "Properties";
+            this.Text = "Properties";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNaam;
+        private System.Windows.Forms.TextBox txtSamen;
+        private System.Windows.Forms.TextBox txtActoren;
+        private System.Windows.Forms.TextBox txtPre;
+        private System.Windows.Forms.TextBox txtPost;
+        private System.Windows.Forms.RichTextBox txtBeschrijving;
+        private System.Windows.Forms.RichTextBox txtUitzondering;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ UseCaseHelper/UseCase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCaseHelper
+{
+    class UseCase
+    {
+        string name;
+        string samenvatting;
+        string preconditie;
+        string beschrijving;
+        string uitzonderingen;
+        string postconditie;
+    }
+}
